18b39f119eb535859ed2951752ee275044d2a1bd
Glaicon <gjpeixer@gmail.com>
2023-12-19 14:29:45 -0500

    update pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {
+  ThirdwebProvider,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+  localWallet,
+  embeddedWallet,
+} from "@thirdweb-dev/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider
+      clientId="604a5a0a55ca96b706c901437960ea6b"
+      supportedWallets={[
+        metamaskWallet(),
+        coinbaseWallet({ recommended: true }),
+        walletConnect(),
+        localWallet(),
+        embeddedWallet(),
+      ]}
+    >
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
